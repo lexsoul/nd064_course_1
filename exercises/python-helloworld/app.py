f4a2a36f24522ca61b1ed53652dc1f77ff43c602
Alexander Ramirez <alexander.ramirez@bairesdev.com>
2021-06-30 13:46:56 -0600

    App using logs

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,33 @@
+import logging
 from flask import Flask
+from flask import json
+
 app = Flask(__name__)
 
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='app.log', encoding='utf-8', level=logging.DEBUG)
 @app.route("/")
 def hello():
+    logging.debug("home endpoint was reached")
     return "Hello World!"
 
+@app.route('/status')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
